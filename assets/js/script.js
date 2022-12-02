6a26870d8bedb8bdefa3856a35667bc2d6529a93
victorlmorales <victor.l.morales2020@gmail.com>
2022-12-01 21:22:45 -0600

    Updated map coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([38.15114045856517, -97.92229780772017], 4);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
