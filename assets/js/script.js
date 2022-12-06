eea06b5e9f64e76e8d6ec07c706d9e452ce93f68
matt <mathewandnana@gmail.com>
2022-12-05 21:28:14 -0600

    adding drop down button

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,3 +68,11 @@ function getLaunches(launchesUrl){
     })
 }
 getLaunches(launchesUrl);
+function show(value) {
+    document.querySelector(".text-box").value = value;
+  }
+  
+  let dropdown = document.querySelector(".dropdown")
+  dropdown.onclick = function() {
+      dropdown.classList.toggle("active")
+  }
